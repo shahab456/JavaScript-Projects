e7e805dd02595ee67cbb92be501487ed9b26c72e
shahab456 <shahab.molouk@gmail.com>
2022-08-24 00:31:03 +0430

    project 5

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -3,4 +3,33 @@ var a ="<h1> Type is : ";
 a+= typeof 41;
 a+="</h1>";
 document.write(a);
+
+var var1 = "<h1>String "+41+"</h1>";
+document.write(var1);
+
+var bool1 = true;
+var bool2 = false;
+var result;
+result = bool1==bool2;
+document.write("<h1>true==false : "+result+"</h1>")
+
+result = bool1===bool2;
+document.write("<h1>true===false :"+result+"</h1>")
+
+result = bool1>bool2;
+document.write("<h1>true&#62;false : "+result+"</h1>")
+
+result = bool1<bool2;
+document.write("<h1>true&#60;false : "+result+"</h1>")
+
+result = bool1&&bool2;
+document.write("<h1>true && false : "+result+"</h1>")
+
+result = bool1||bool2;
+document.write("<h1>true || false : "+result+"</h1>")
+
+result = !bool2;
+document.write("<h1>!false : "+result+"</h1>")
+
+
 document.close();
